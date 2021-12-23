58911f80d4dc72f0d9a755528e1995f1cc3249c3
Tchilo <tchiloross@gmail.com>
2021-12-23 13:58:11 +0200

    Modify student, rental  files

--- student.rb
+++ student.rb
@@ -1,11 +1,17 @@
 require_relative('./person')
 
 class Student < Person
-  def initialize(*args, classroom, **kwargs)
+  attr_reader :classroom
+
+  def initialize(*args, classroom: 'Unknown', **kwargs)
     super(*args, **kwargs)
     @classroom = classroom
   end
 
+  def classroom(classroom)
+    classroom.student.push(self) unless classroom.students.include?(self)
+  end
+  
   def play_hooky
     "¯\(ツ)/¯"
   end
