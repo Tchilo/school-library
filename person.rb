bcd124536c4e1a72cae518d6e1e7a92f59e8f912
Tchilo <tchiloross@gmail.com>
2021-12-23 13:48:04 +0200

    Modify person  file

--- person.rb
+++ person.rb
@@ -7,11 +7,16 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
   attr_accessor :name, :age
   attr_reader :id
 
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
+  
   def can_use_services?
     of_age? || @parent_permission
   end
